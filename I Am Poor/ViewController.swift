beb20e592820f65c6d514d415aedca320333a497
Daisugi <volble124@gmail.com>
2019-10-22 22:58:03 +0900

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 杉原大貴 on 2019/10/22.
+//  Copyright © 2019年 Daiki Sugihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
